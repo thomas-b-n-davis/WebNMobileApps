d55071b8e7a118ba527c59ba65171e2528da23d7
thomas davis <thomas.b.n.davis@outlook.com>
2019-12-17 17:12:25 +0100

    ui routes and interface added

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,24 @@
  */
 var express = require('express');
 var router = express.Router();
+const path = require('path');
+
+
+router.get('/',function(req,res){
+  res.render('index');
+});
+
+router.get('/contact',function(req,res){
+  res.render('contact');
+});
+
+router.get('/shop',function(req,res){
+  res.render('shop');
+});
+
+router.get('/product',function(req,res){
+  res.render('product');
+});
 
 
 //router.get('/product/findAll', user.getOne);
