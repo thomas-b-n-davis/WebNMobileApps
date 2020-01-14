41d3bbdfb972d1b28a4370d3b688e18036447d1e
abubaker <abubaker.tts@gmail.com>
2020-01-14 23:03:37 +0100

    chagne config.js file

--- Controller/UserController.js
+++ Controller/UserController.js
@@ -2,7 +2,6 @@ services = require("../Services/service_index");
 
 exports.createUser  = (req, res, next) =>{
   var data=req.body;
-  console.log(data);
   services.User.createUserQuery(
     {
     name : data.name,
